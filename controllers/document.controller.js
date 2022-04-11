bb8af46bfebe1158f1b33d9cceee9c99c97f7a35
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-12 03:58:35 +0530

    doc get query set

--- controllers/document.controller.js
+++ controllers/document.controller.js
@@ -6,7 +6,11 @@ module.exports={
     async getDocumenByBranch(req,res){
         try{
             var branch= req.query.branch;
-            var [result,metadata] = await sequelize.query(`select d.uuid as Doc_id,d.doc_name as Doc, b.name as Branch from documents d, branches b where d.branch_id = b.uuid and b.name='${branch}'`)
+            var [result,metadata] = await sequelize.query(
+                `select d.uuid as doc_id,d.doc_name as doc,d.type as doc_type,du.customer_id,u.name as customer_name
+                 from documents d, branches b,users u,documentusers du 
+                 where d.branch_id = b.uuid and b.name='${branch}' and du.doc_id=d.uuid and du.is_current_user=${true} and du.customer_id = u.uuid`
+                 )
             console.log(result)
             res.status(200).send(result);
         }catch(e){
