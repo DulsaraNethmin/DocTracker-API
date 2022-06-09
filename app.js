61c8a54bfe92975101072be76cd8aae04d606fdb
Chamodhya <chamodhyamanawathilake@gmail.com>
2022-06-09 13:17:22 +0530

    orgroute

--- app.js
+++ app.js
@@ -2,21 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const {sequelize} = require('./models');
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
 require('dotenv').config();
 app.use(cors(corsOptions));
 
 app.use(express.json());
-
+require('./routes')(app);
 app.use(express.urlencoded({ extended: true }));
 
-require('./routes')(app);
+//routers
+const router=require('./routes/orgRouter');
+
+try{
+  app.use('/api/organization',router);
+} catch(e)
+{
+    res.json(e);
+}
+
+
+1
 
 app.get("/", (req, res) => {
   res.send("Welcome to DocTracker Api");
 });
+
 
 
 const PORT = process.env.PORT || 8080;
---
+++ routes/userRoutes.js
@@ -0,0 +1,4 @@
+// const usercontroller=require('../controllers/user.controller');
+// const router=require('express').Router;
+// router.get("/getuser",usercontroller.getAllUser);
+// module.exports=router;
