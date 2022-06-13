cdf484b45aac80be393ab4b685c6467bbcbe24d3
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-06-13 20:20:06 +0530

    job model created using sequelize

---
+++ REST/migrations/20220613144930-create-job.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('jobs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        type: Sequelize.UUID
+      },
+      is_completed: {
+        type: Sequelize.BOOLEAN
+      },
+      branch_id: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('jobs');
+  }
+};
