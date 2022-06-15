09188f9134fea59d52f58431fd38cc45d7614848
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-15 13:47:49 +0530

    Branch controller, send data with status code

--- REST/controllers/branch.controller.js
+++ REST/controllers/branch.controller.js
@@ -3,14 +3,14 @@ const branch = require('../models').Branch
 module.exports={
     async addBranch(req,res){
         try{
-            var newdata= await branch.create({
+            var newData= await branch.create({
                 name:req.body.name,
                 number:req.body.number,
                 town:req.body.town,
                 street:req.body.street,
                 organization_id:req.body.organization_id,
             })
-            res.status(200).send('new branch created')
+            res.status(200).send(newData)
         }catch(e){
             console.log('an error occured '+e)
             res.status(500).send("server error")
