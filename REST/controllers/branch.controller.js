058098fa466ed9d0d18cdaf583a7dcd0213b3c50
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-15 15:27:03 +0530

    created getAllBranches in branches.controllers

--- REST/controllers/branch.controller.js
+++ REST/controllers/branch.controller.js
@@ -17,6 +17,21 @@ module.exports={
         }
     },
 
+    async getAllBranches(req, res) {
+        console.log("request come");
+        try {
+          var [result, metadata] = await sequelize.query(
+            `select u.uuid ,u.name as name ,u.email,u.username,u.role,b.name as branch,b.uuid as branchId ,u.image_url as image_url
+                    from users u, branches b 
+                    where u.branch_id=b.uuid and u.branch_id='${req.query.branch_id}'`
+          );
+          res.status(200).send(result);
+        } catch (e) {
+          console.log("an error occured " + e);
+          res.status(500).send("Server error");
+        }
+      },
+
     async deleteBranch(req,res){
         try{
             uuid=req.query.uuid
