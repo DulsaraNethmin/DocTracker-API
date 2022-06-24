a189b05323c2e293e13a8767329fa04ec98a6046
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-06-24 19:42:12 +0530

    job model

--- REST/controllers/job.controller.js
+++ REST/controllers/job.controller.js
@@ -12,7 +12,7 @@ module.exports={
                 branch_id:req.body.branch_id,
                 deliverer_id:req.body.deliverer_id,
                 customer_id:req.body.customer_id,
-                is_completed:false
+                is_completed:0
             });
             //console.log(result);
             res.status(201).send(result);
@@ -30,7 +30,7 @@ module.exports={
                 `select j.uuid as job_id,j.customer_id as customer_id,u.name as customer_name, j.is_completed as is_job_completed, d.uuid as delivery_id, d.doc_id as doc_id, 
                  doc.doc_name as doc_name, d.end_customer_id as end_custmer_id, uu.name as end_customer_name, d.is_completed as is_delivery_completed
                 from jobs j, users u, deliveries d, documents doc,users uu
-                where j.branch_id='${branch_id}' and j.uuid=d.job_id and j.customer_id = u.uuid and  j.is_completed=${false} and  d.doc_id=doc.uuid and uu.uuid=d.end_customer_id`
+                where j.branch_id='${branch_id}' and j.uuid=d.job_id and j.customer_id = u.uuid and  j.is_completed=${0} and  d.doc_id=doc.uuid and uu.uuid=d.end_customer_id`
             );
             console.log(result);
             res.status(201).send(result);
@@ -47,7 +47,7 @@ module.exports={
             var response=await delivery.create({
                 "end_customer_id":deliveries[i]["end_customer"],
                 "doc_id":deliveries[i]["doc_id"],
-                "is_completed":false,
+                "is_completed":0,
                 "job_id":req.body.job_id
             });
         }
@@ -65,7 +65,7 @@ module.exports={
         var [result,metadata]=await sequelize.query(
             `select count(uuid) as count
             from deliveries 
-            where doc_id="${doc_id}" and is_completed =${false}`
+            where doc_id="${doc_id}" and is_completed =${0}`
         );
         console.log(result[0].count);
         res.status(200).send(result);
