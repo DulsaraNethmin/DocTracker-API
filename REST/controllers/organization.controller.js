036d9121f2410a69effacd5544bda35ab26c2276
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-26 03:12:30 +0530

    getSingleBranchOwner controller in organizations

--- REST/controllers/organization.controller.js
+++ REST/controllers/organization.controller.js
@@ -50,6 +50,23 @@ module.exports={
         console.log(result)
       },
 
+      async getSingleBranchOwner(req, res) {
+        console.log("request come");
+        console.log(req.query.branch_id);
+        try {
+          var [result, metadata] = await sequelize.query(
+            `select b.name as b_name,b.number as b_number,b.town as b_town,b.street as b_street,u.name,u.username,u.password, u.email, u.telephone
+                          from users u, branches b 
+                          where u.branch_id=b.uuid and b.uuid='${req.query.branch_id}' and u.role='Branch Owner'`
+          );
+          res.status(200).send(result);
+        } catch (e) {
+          console.log("an error occured " + e);
+          res.status(500).send("Server error");
+        }
+        console.log(result);
+      },
+
     // async updateOrganization(String owner_id){
     //     try{
     //         var newData= await organization.update(
